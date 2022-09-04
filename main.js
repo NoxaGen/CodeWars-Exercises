be9eefa44fe76ef8f43bf2e28c9f395285435002
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-09-04 14:39:44 +0200

    added new exercise

--- main.js
+++ main.js
@@ -105,6 +105,14 @@ function solution(str) {
     return evenStrs;
 }
 
-//note: im proud, because i got leveled from 8kyu on 6kyu :D
-//completed
-//////////////////////////////////////////////////////
+//Your task is to make a function that can take any non-negative integer 
+//as an argument and return it with its digits in descending order. 
+//Essentially, rearrange the digits to create the highest possible number.
+//Examples:
+//Input: 42145 Output: 54421
+//Input: 145263 Output: 654321
+//Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+    //...
+}
