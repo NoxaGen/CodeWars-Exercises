1de3cadfb5b4a16703d5fddc0c024fb91798a6ab
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-09-09 16:55:33 +0200

    completed personalized message task

--- main.js
+++ main.js
@@ -310,3 +310,14 @@ function removeChar(str) {
 };
 
 //completed
+
+//Personalized Message
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+function greet(name, owner) {
+    if (name === owner) {
+        return 'Hello boss'
+    } else return 'Hello guest'
+}
+
+//completed, to easy
