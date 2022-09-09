0a0c4dedaa4c89fb2906ef8ccd5fcd0828aa1b48
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-09-09 16:41:24 +0200

    completed removeChar function

--- main.js
+++ main.js
@@ -295,3 +295,18 @@ function find_average(array) {
         return (sum / array.length);
     }
 }
+
+//removeChar
+
+//It's pretty straightforward. Your goal is to create a function that removes the first 
+//and last characters of a string. You're given one parameter, the original string. 
+//You don't have to worry with strings with less than two characters.
+
+function removeChar(str) {
+    let strArr = [...str]
+    strArr.pop();
+    strArr.shift();
+    return strArr.join("");
+};
+
+//completed
