cb2497a9733a673514de168f9ebd75281bcf9bc7
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-09-15 18:01:01 +0200

    trying to solve population function

--- main.js
+++ main.js
@@ -440,3 +440,46 @@ function pigIt2(str) {
 function numberToString(num) {
     return num.toString();
 }
+
+//In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases
+// by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. 
+//How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+//1st year 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+//2nd year 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+//3rd year 1141 + 1141 * 0.02 + 50 => 1213 (It will need 3 entire years.)
+//More generally given parameters: (p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass))
+//the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+//aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+//Examples:
+//nb_year(1500, 5, 100, 5000) -> 15
+//nb_year(1500000, 2.5, 10000, 2000000) -> 10 Note: Don't forget to convert the percent parameter as a percentage
+// in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+//p0 - population of beggining of the yeah
+//percent - of increased population
+//aug - number of inhabitants coming to live in town
+//p - in this place i need to put number of citizens i want to target, they function need to calc in years and return int
+
+function nbYear(p0, percent, aug, p) {
+    let result = p0 * (percent / 100) + aug;
+    let test = 0;
+    let i = Math.round(p / result);
+    console.log(result)
+
+    if (result < p) {
+        for (i; i < )
+    }
+
+    console.log(p / result);
+    console.log(Math.round(p / result));
+
+
+
+    if (p > result) {
+        console.log('need more then 1 year')
+    } else {
+        console.log('need less then one year')
+    }
+
+
+}
