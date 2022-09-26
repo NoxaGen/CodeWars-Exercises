a9683526b77c1880c15d147246318eb3be7fa6f3
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-09-26 13:59:09 +0200

    completed needle function

--- main.js
+++ main.js
@@ -471,3 +471,15 @@ function friend(friends) {
 }
 
 //completed
+
+// Needle in the Haystack
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+//"found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+    let needle = haystack.indexOf('needle');
+    return `found the needle at position ${needle}`
+}
+
+//completed
