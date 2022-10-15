976f238b4561e7e2306ed01836a1aee36017a69b
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-10-15 17:37:33 +0200

    i started with common alphabet need in exercises

--- main.js
+++ main.js
@@ -677,3 +677,20 @@ function getDivisorsCnt(n) {
 
 }
 //completed, modulo rox
+
+//https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
+//MORSE CODE DECODER part 1.
+
+//test in node.js are only for HEY JUDE, so i wont use my whole alphabet to not wasting time
+
+decodeMorse = function (morseCode) {
+    const alphabet = {
+        h: '....',
+        e: '.',
+        y: '-.--',
+        j: '.---',
+        u: '..-',
+        d: '-..',
+        e: '.'
+    };
+}
