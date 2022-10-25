47f28a5b43fe07ce7d0f153943c82faf2cb2bb1c
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-10-25 16:10:25 +0200

    completed facebook-likes function

--- main.js
+++ main.js
@@ -776,3 +776,23 @@ function decodeMorse(morseCode) {
 }
 
 //completed, all working - but the tests in node are broken on codewars, so i dodnt get the points...
+
+//facebook like system
+//You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items.
+// We want to create the text that should be displayed next to such an item.
+
+function likes(names) {
+    if (names.length === 0) {
+        return `no one likes this`
+    } else if (names.length === 1) {
+        return `${names[0]} likes this`
+    } else if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else if (names.length > 3) {
+        return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+    }
+}
+
+//completed on the first run all tests :) pretty easy for 6kyu
