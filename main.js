cac480df1c698e3a71342add7c6bde4ff23d777c
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2023-01-10 15:04:22 +0100

    trying to find solution with knows methods

--- main.js
+++ main.js
@@ -1232,25 +1232,16 @@ function high(x) {
 
     let spreadSentence = [...x.split(' ')];
     let checkLettersValue = [];
+    const alphabetWithNumberValues = [a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21, v = 22, w = 23, x = 24, y = 25, z = 26];
+    const alphabetWithStrings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     spreadSentence.forEach(sentence => {
-        checkLettersValue.push(sentence.split(''))
-    });
-
-    checkLettersValue.forEach(letter => {
-        console.log(letter)
+        checkLettersValue.push(sentence.split('')) s
     });
 
 
-    console.log(spreadSentence)
-
-    console.log(checkLettersValue)
-
-    const arr = [a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21, v = 22, w = 23, x = 24, y = 25, z = 26];
-    console.log(arr[0].valueOf())
-    console.log(arr[5].valueOf())
 
 
-    console.log(a.valueOf())
+
 
 }
